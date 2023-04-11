22d0e4d07c74897b3fa67022dba0dd20d076b405
huangdadong <377080411@qq.com>
2023-04-11 12:58:36 +0800

    ccem safeloop work, rce cap under check

--- examples/benchmarks/run_experiment_grid.py
+++ examples/benchmarks/run_experiment_grid.py
@@ -75,13 +75,9 @@ if __name__ == '__main__':
     # naive_lagrange_policy = ['PPOLag', 'TRPOLag', 'RCPO', 'OnCRPO', 'PDO']
     # first_order_policy = ['CUP', 'FOCOPS', 'P3O']
     # second_order_policy = ['CPO', 'PCPO']
-    model_base_policy = ['PETS', 'LOOP']
+    model_base_policy = ['PETS', 'LOOP', 'CCEM', 'SafeLOOP', 'CAPPETS', 'RCEPETS']
     # Set the environments.
     mujoco_envs = [
-        'SafetyAntVelocity-v4',
-        'SafetyHopperVelocity-v4',
-        'SafetyHumanoidVelocity-v4',
-        'SafetyWalker2dVelocity-v4',
         'SafetyPointGoal1-v0-modelbased',
         'SafetyCarGoal1-v0-modelbased',
         'SafetyAntGoal1-v0-modelbased',
@@ -90,7 +86,7 @@ if __name__ == '__main__':
 
     # Set the device.
     avaliable_gpus = list(range(torch.cuda.device_count()))
-    gpu_id = [5, 6, 7]
+    gpu_id = [3, 4, 5, 6, 7]
     # if you want to use CPU, please set gpu_id = None
     # gpu_id = None
 
@@ -102,9 +98,9 @@ if __name__ == '__main__':
     eg.add('logger_cfgs:use_wandb', [True])
     # eg.add('train_cfgs:vector_env_nums', [4])
     # eg.add('train_cfgs:torch_threads', [1])
-    eg.add('algo_cfgs:plan_horizon', [10, 15, 20, 25])
+    # eg.add('algo_cfgs:plan_horizon', [10, 15, 20, 25])
     eg.add('train_cfgs:total_steps', [1000000])
-    eg.add('seed', [0])
+    eg.add('seed', [0, 5, 10])
     # total experiment num must can be divided by num_pool
     # meanwhile, users should decide this value according to their machine
     eg.run(train, num_pool=12, gpu_id=gpu_id)
@@ -114,6 +110,6 @@ if __name__ == '__main__':
     # or you can just specify how many values you want to compare in single graph at most,
     # and the function will automatically generate all possible combinations of the graph.
     # but the two mode can not be used at the same time.
-    eg.analyze(parameter='env_id', values=None, compare_num=6, cost_limit=25)
+    eg.analyze(parameter='env_id', values=None, compare_num=6, cost_limit=1)
     eg.render(num_episodes=1, render_mode='rgb_array', width=256, height=256)
     eg.evaluate(num_episodes=1)
